ffe66d3e9d257a8996c6944dfe6dd077743bf347
ben garside <ben.garside@bsigroup.com>
2016-01-15 11:37:10 +0000

    main changes

--- export_repo_issues_to_csv.py
+++ export_repo_issues_to_csv.py
@@ -16,27 +16,38 @@ AUTH = (GITHUB_USER, GITHUB_PASSWORD)
 
 
 def write_issues(response):
-    "output a list of issues to csv"
+    '''output a list of issues to csv'''
     if not r.status_code == 200:
         raise Exception(r.status_code)
     for issue in r.json():
-        # labels = issue['labels']
-        # for label in labels:
-            # if label['name'] == "Client Requested":
+        labels = ''
+        for label in issue['labels']:
+            labels = labels + ', ' + label['name']
+        labels = labels[2:]
+        if issue['milestone'] is None:
+            issue_milestone = ''
+        else:
+            issue_milestone = issue['milestone']['title']
+        if issue['assignee'] is None:
+            issue_assignee = ''
+        else:
+            issue_assignee = issue['assignee']['login']
         csvout.writerow([issue['number'],
-                        issue['title'].encode('utf-8'),
-                        issue['body'].encode('utf-8'),
-                        issue['created_at'],
-                        issue['updated_at']])
+                         issue['title'],
+                         issue['user']['login'],
+                         issue['created_at'],
+                         issue['updated_at'],
+                         labels,
+                         issue['state'],
+                         issue_milestone,
+                         issue_assignee])
 
 r = requests.get(ISSUES_FOR_REPO_URL, auth=AUTH)
 csvfile = '%s-issues.csv' % (REPO.replace('/', '-'))
-print('sd')
-csvout = csv.writer(open(csvfile, 'wb'))
-csvout.writerow(('id', 'Title', 'Body', 'Created At', 'Updated At'))
+csvout = csv.writer(open(csvfile, 'w', newline=''))
+csvout.writerow(('id', 'Title', 'Opened By', 'Created At', 'Updated At', 'Labels', 'State', 'Milestone', 'Assignee'))
 write_issues(r)
-
-# more pages? examine the 'link' header returned
+#more pages? examine the 'link' header returned
 if 'link' in r.headers:
     pages = dict(
         [(rel[6:-1], url[url.index('<')+1:-1]) for url, rel in
